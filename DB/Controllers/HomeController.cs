9cc5537bc8a246de82e9a1e319426ba7e9bb5e3b
LK <luaikamal.0.1@gmail.com>
2016-11-02 17:27:02 -0400

    DB

---
+++ DB/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DB.Models;
+
+namespace DB.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Color color)
+        {
+            Color? oldColor = Session["color"] as Color?;
+            if (oldColor != null)
+            {
+                Votes.ChangeVote(color, (Color)oldColor);
+            }
+            else
+            {
+                Votes.RecordVote(color);
+            }
+            ViewBag.SelectedColor = Session["color"] = color;
+            return View();
+        }
+    }
+}
